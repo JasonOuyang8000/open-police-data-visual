691bafb99d52252efa1ac84da45c211de3e41c92
Jason Ouyang <jasonouyang8000@gmail.com>
2021-07-24 05:04:55 -0700

    setup with eslint for optimal code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
-    return (
-        <div>
+  const getResponse = async () => {
+    try {
+      const response = await axios.get('https://openpolice.org/api/all/complaint/xml?limit=20&start=0', { headers: { 'Access-Control-Allow-Origin': '*' } });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getResponse();
+  }, []);
 
-
-        </div>
-    )
-}
-
+  return (
+    <div />
+  );
+};
 
 export default App;
