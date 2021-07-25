3f49b538479bbe77022692ac3a24cae0bd4281d8
Jason Ouyang <jasonouyang8000@gmail.com>
2021-07-24 17:14:58 -0700

    setup for data collection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import convert from 'xml-js';
 
 const App = () => {
   const getResponse = async () => {
     try {
-      const response = await axios.get('https://openpolice.org/api/all/complaint/xml?limit=20&start=0', { headers: { 'Access-Control-Allow-Origin': '*' } });
-      console.log(response);
+      const response = await axios.get(process.env.API_URL, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/xml',
+        },
+        params: {
+          start: 0,
+          limit: 20,
+        },
+      });
+      const data = JSON.parse(
+        convert.xml2json(response.data, { compact: true, spaces: 2 }),
+      );
+      console.log(data);
     } catch (error) {
       console.log(error);
     }
