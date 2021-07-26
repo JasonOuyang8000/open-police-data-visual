c489edff632ee8be30d41521187344dfc874e40a
Jason Ouyang <jasonouyang8000@gmail.com>
2021-07-26 01:31:53 -0700

    added charts to nivo graph

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import convert from 'xml-js';
+import React, { useEffect, useState } from 'react';
+import data from './assets/data.json';
 
 const App = () => {
-  const getResponse = async () => {
-    try {
-      const response = await axios.get(process.env.API_URL, {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/xml',
-        },
-        params: {
-          start: 0,
-          limit: 20,
-        },
-      });
-      const data = JSON.parse(
-        convert.xml2json(response.data, { compact: true, spaces: 2 }),
-      );
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // eslint-disable-next-line no-unused-vars
+  const [totalData, setTotalData] = useState([]);
+
   useEffect(() => {
-    getResponse();
+    setTotalData(data);
   }, []);
 
+  console.log(data);
   return (
     <div />
   );
